4f5e6d88f18872e3cd7afe1e46cd5f494ac3076e
Pawan Mittal <pmittal@bestpeers.com>
2018-07-18 14:39:17 +0530

    Added react-toastify

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 
 const App = () => {
   return (
     <div className="App">
+      <ToastContainer />
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={LoginForm} />
