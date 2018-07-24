270c747092299c603830e2324e9c295435c59d7f
Pawan Mittal <pmittal@bestpeers.com>
2018-07-24 16:26:44 +0530

    Updated Header with nav-bar

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,27 +1,34 @@
-import axios from 'axios';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 class Header extends React.Component {
   handleLogout() {
     if (this.props.user) {
       window.location.reload();
-      axios.get('http://localhost:4000/Logout');
+      this.props.logout();
     } else {
       alert("You are alredy logged out!");
     }
   }
   render() {
     return (
-      <div className="header">
-        <h3>Task Management system</h3>
-        <span>
-        {
-          this.props.user &&
-          <button className="pull-right btn btn-sm btn-warning" onClick={() => { this.handleLogout(); }}>Logout</button>
-        }
-        </span>
-      </div>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Task Management System</a>
+          </div>
+          {
+            this.props.user &&
+            <ul className="nav navbar-nav">
+              <li className="active"><Link to="/Dashboard">Home</Link></li>
+              <li><Link to="/Profile">Profile</Link></li>
+              <li><Link to="/TaskManagement">Tasks</Link></li>
+              <li className="pull-right"><button className="btn btn-sm btn-warning" onClick={() => { this.handleLogout(); }}>Logout</button></li>
+            </ul>
+          }
+        </div>
+      </nav>
     )
   }
 }
