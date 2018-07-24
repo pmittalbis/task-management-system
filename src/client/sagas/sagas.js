b090867eb52f1060bff7c6f757a64c58bbed9abe
Pawan Mittal <pmittal@bestpeers.com>
2018-07-24 16:30:28 +0530

    Added update profile API call

--- src/client/sagas/sagas.js
+++ src/client/sagas/sagas.js
@@ -1,92 +1,20 @@
 import { all, takeEvery, put } from 'redux-saga/effects';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import {
-  ASSIGN_TASK,
-  LOGIN,
-  GET_USERS,
+import { ASSIGN_TASK,
   GET_AUTH_USER,
-  SET_AUTH_USER,
   GET_TASKS,
+  GET_USERS,
+  LOGIN,
+  LOGOUT,
+  SET_AUTH_USER,
   SET_TASKS,
   SET_USERS,
   SIGNUP,
-} from '../constants';
+  UPDATE_PROFILE, } from '../constants';
 
 function notify(msg) {
   toast(msg);
-}
-
-export function* takeSignup() {
-  yield takeEvery(SIGNUP, signupUser);
-}
-
-function* signupUser(action) {
-  yield axios.post('http://localhost:4000/Signup', action.signupDetails)
-   .then((response) => {
-     if (typeof response.data === "object") {
-       notify("You have successfully signed up.");
-       setTimeout(() => { action.thisContext.props.history.push("/") }, 2000)
-       return response.data
-     } else {
-       notify(response.data);
-       return false;
-     }
-   })
-   .catch((err) => { console.log(err) });
-}
-
-export function* takeLogin() {
-  yield takeEvery(LOGIN, loginUser);
-}
-
-function* loginUser(action) {
-  yield axios.post('http://localhost:4000/Login', {
-    email: action.email,
-    password: action.password,
-  })
-   .then((response) => {
-     if (typeof response.data === "object") {
-       notify("Welcome " + response.data.name);
-       setTimeout(() => { action.thisContext.props.history.push("/Dashboard") }, 2000)
-       return response.data
-     } else {
-       this.setState({
-         message: response.data,
-       });
-     }
-   })
-   .catch((err) => { console.log(err) });
-}
-
-export function* takeGetUsers() {
-  yield takeEvery(GET_USERS, getUsers);
-}
-
-function* getUsers(action) {
-  const users = yield axios.get('http://localhost:4000/GetUsers')
-   .then((response) => {
-     return response.data
-   })
-   .catch((err) => { console.log(err) });
-   if (users) {
-     yield put({ type: SET_USERS, users });
-   }
-}
-
-export function* takeGetTasks() {
-  yield takeEvery(GET_TASKS, getTasks);
-}
-
-function* getTasks(action) {
-  const tasks = yield axios.get(`http://localhost:4000/GetTasks/${action.userId}`)
-   .then((response) => {
-     return response.data
-   })
-   .catch((err) => { console.log(err) });
-   if (tasks) {
-     yield put({ type: SET_TASKS, tasks });
-   }
 }
 
 export function* takeAssignTask() {
@@ -119,13 +47,113 @@ function* getAuthUser(action) {
    yield put({ type:SET_AUTH_USER, user })
 }
 
+export function* takeGetTasks() {
+  yield takeEvery(GET_TASKS, getTasks);
+}
+
+function* getTasks(action) {
+  const tasks = yield axios.get(`http://localhost:4000/GetTasks/${action.userId}`)
+   .then((response) => {
+     if (typeof response.data === "object") {
+       return response.data
+     } else {
+       notify(response.data);
+     }
+   })
+   .catch((err) => { console.log(err) });
+   if (tasks) {
+     yield put({ type: SET_TASKS, tasks });
+   }
+}
+
+export function* takeGetUsers() {
+  yield takeEvery(GET_USERS, getUsers);
+}
+
+function* getUsers(action) {
+  const users = yield axios.get('http://localhost:4000/GetUsers')
+   .then((response) => {
+     if (typeof response.data === "object") {
+       return response.data
+     } else {
+       notify(response.data);
+     }
+   })
+   .catch((err) => { console.log(err) });
+   if (users) {
+     yield put({ type: SET_USERS, users });
+   }
+}
+
+export function* takeLogin() {
+  yield takeEvery(LOGIN, loginUser);
+}
+
+function* loginUser(action) {
+  yield axios.post('http://localhost:4000/Login', {
+    email: action.email,
+    password: action.password,
+  })
+   .then((response) => {
+     if (typeof response.data === "object") {
+       notify("Welcome " + response.data.name);
+       setTimeout(() => { action.thisContext.props.history.push("/Dashboard") }, 2000)
+       return response.data
+     } else {
+       notify(response.data);
+     }
+   })
+   .catch((err) => { console.log(err) });
+}
+
+export function* takeLogout() {
+  yield takeEvery(LOGOUT, logoutUser);
+}
+
+function* logoutUser(action) {
+  yield axios.get('http://localhost:4000/Logout')
+   .catch((err) => { console.log(err) });
+}
+
+export function* takeSignup() {
+  yield takeEvery(SIGNUP, signupUser);
+}
+
+function* signupUser(action) {
+  yield axios.post('http://localhost:4000/Signup', action.signupDetails)
+   .then((response) => {
+     if (typeof response.data === "object") {
+       notify("You have successfully signed up.");
+       setTimeout(() => { action.thisContext.props.history.push("/") }, 2000)
+       return response.data
+     } else {
+       notify(response.data);
+     }
+   })
+   .catch((err) => { console.log(err) });
+}
+
+export function* takeUpdateProfile() {
+  yield takeEvery(UPDATE_PROFILE, updateProfile);
+}
+
+function* updateProfile(action) {
+  yield axios.put(`http://localhost:4000/UploadProfile/${action.userId}`, action.formData)
+   .then((res) => {
+     console.log("Updated user ", res.data)
+   })
+   .catch((err) => { console.log(err) });
+}
+
 export default function* rootSaga() {
   yield all([
-    takeSignup(),
-    takeLogin(),
-    takeGetUsers(),
-    takeGetTasks(),
     takeAssignTask(),
     takeGetAuthUser(),
+    takeGetTasks(),
+    takeGetUsers(),
+    takeLogin(),
+    takeLogout(),
+    takeSignup(),
+    takeUpdateProfile(),
   ]);
 }
